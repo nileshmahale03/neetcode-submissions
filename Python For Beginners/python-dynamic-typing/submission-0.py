0b12e2537b24b72511e782ef627eaf62b0b2c82d
Nilesh Mahale <nileshmahale03@gmail.com>
2026-08-08 09:11:09 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 33
+print(type(variable))
+variable = 3.14
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Grand"
+print(type(variable))
+variable = [2, 4, 6, 8]
+print(type(variable))
