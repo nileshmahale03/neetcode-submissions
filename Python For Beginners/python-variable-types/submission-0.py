880e48640bc8a131dc41d5381a8853739ff5738d
Nilesh Mahale <nileshmahale03@gmail.com>
2026-08-08 09:07:34 -0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 33
+float_type   = 3.14
+bool_type    = False
+string_type  = "Canyon"
+list_type    = [3, 2, 1]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
